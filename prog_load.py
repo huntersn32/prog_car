0db761f02d7237e9b42ef34584ea7b1608618f25
huntersn32 <huntersn32@gmail.com>
2018-09-26 17:33:04 +0300

    Initial Commit1

--- prog_load.py
+++ prog_load.py
@@ -11,22 +11,22 @@ class ExampleApp(QtWidgets.QMainWindow, Design_load.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
         self.loadbtnReis.clicked.connect(self.load_Db_reis) #Выполнить функцию load_Db_reis
-        
+        self.loadbtnCash.clicked.connect(self.load_Db_cash) #Выполнить функию cash
 
     def load_Db_reis(self):
         
-        date_L = self.dateLoad.selectedDate()
-        date_L = date_L.toString('dd.MM.yyyy')
+        date_L = self.dateLoad.selectedDate() # дата загрузки
+        date_L = date_L.toString('dd.MM.yyyy') # перевод в читаемый формат
         L = date_L
-        country_load = self.cmbLoad.currentText()
-        date_U = self.dateUpload.selectedDate()
-        date_U = date_U.toString('dd.MM.yyyy')
+        country_load = self.cmbLoad.currentText() # страна загрузки
+        date_U = self.dateUpload.selectedDate() # дата выгрузки
+        date_U = date_U.toString('dd.MM.yyyy') # перевод в читаемый формат
         U = date_U
-        country_upload = self.cmbUpload.currentText()
-        car = self.cmbCarnumber.currentText()
-        fio = self.cmbDriverfio.currentText()
-        cash = self.lineCashdrive.text()
-        val = self.cmbVal.currentText()
+        country_upload = self.cmbUpload.currentText() # страна выгрузки
+        car = self.cmbCarnumber.currentText() # № машины
+        fio = self.cmbDriverfio.currentText() # ФИО водителя
+        cash = self.lineCashdrive.text() # Стоимость перевозки
+        val = self.cmbVal.currentText() # валюта
         pool = (date_L,country_load,date_U,country_upload,
                 car,fio,cash,val)
 
@@ -46,6 +46,23 @@ Counry_upload,Car_number,Fraht,Fraht_val,Fio_driver)
                                  
         conn.commit()
         conn.close()
+
+    def load_Db_cash(self):
+        dateload = self.dateLoadCash.selectedDate() # Дата загрузки
+        dateload = dateload.toString('dd.MM.yyy')
+        carnumbercash = self.cmb_car_cash.currentText() # номер машины
+        fiocash = self.fiocmbCash.currentText() # ФИО водителя
+        casheur = self.cmbCashLoadEuro.text() # получено евро
+        cahsrub = self.cmbCashLoadRub.text() # получено рублей
+        spenteuro = self.cmbCashSpentEuro.text() # потрачено евро
+        spentrub = self.cmbCashSpentRub.text() # потрачено руб
+        cashKm = self.CashKm.text() # пройденный километраж
+        grosscash = self.CashGross.text() # вес груза
+        esp = int(grosscash)*0,4
+        summa = (cashKm*esp)/100
+        self.lcdCash.intValue(summa)
+        print(summa)
+        rashodDT = summa
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
